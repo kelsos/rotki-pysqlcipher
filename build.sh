27d715224d74c0439d36b50fb9ee4c656146b059
Konstantinos Paparas <kelsos86@gmail.com>
2022-06-02 19:07:56 +0200

    remove cat

--- build.sh
+++ build.sh
@@ -17,7 +17,5 @@ then
     git apply ../patches/pysqlcipher3_linux.diff
 fi
 
-cat setup.py
-
-cp ..sqlcipher/.libs/libsqlcipher.so ../pysqlcipher3
+cp ../sqlcipher/.libs/libsqlcipher.so ../pysqlcipher3
 cp /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 ../pysqlcipher
