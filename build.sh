ccf8af123994e71e2594ac84c57f46ff7bcaf56c
Konstantinos Paparas <kelsos86@gmail.com>
2022-06-03 15:09:16 +0200

    update

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 WORKDIR=$PWD
 BUILDDIR=/tmp/pybuild
+SQLCIPHER_DIR="$BUILDDIR/sqlcipher"
+PYSQLCIPHER_DIR="$BUILDDIR/pysqlcipher3"
 
 if [[ -d "$BUILDDIR" ]]; then
     echo "Warning $BUILDDIR exists"
@@ -10,9 +12,9 @@ fi
 
 # Go into the directory and build sqlcipher
 echo "Creating sqlcipher amalgamation";
-mkdir -p "$BUILDDIR/sqlcipher"
+mkdir -p "$SQLCIPHER_DIR"
 cp -R sqlcipher "$BUILDDIR"
-cd "$BUILDDIR/sqlcipher/" || exit 1
+cd "$SQLCIPHER_DIR" || exit 1
 
 ./configure \
   --enable-tempstore=yes \
@@ -23,14 +25,19 @@ make sqlite3.c
 
 cd "$WORKDIR" || exit 1
 cp -R pysqlcipher3 "$BUILDDIR"
-cd "$BUILDDIR/pysqlcipher3" || exit 1
+cd "$PYSQLCIPHER_DIR" || exit 1
 echo "Applying pysqlcipher3 setup patch"
 patch < "$WORKDIR/patches/pysqlcipher3_linux.diff"
 
-if [[ ! -d "$BUILDDIR/pysqlcipher3/amalgamation" ]]; then
-    mkdir "$BUILDDIR/pysqlcipher3/amalgamation"
+if [[ ! -d "$PYSQLCIPHER_DIR/amalgamation" ]]; then
+    mkdir "$PYSQLCIPHER_DIR/amalgamation"
 fi
 
-cp "$BUILDDIR/sqlcipher/sqlite3.c" $BUILDDIR/pysqlcipher3
-cp "$BUILDDIR/sqlcipher/sqlite3.h" $BUILDDIR/pysqlcipher3
+cp "$BUILDDIR/sqlcipher/sqlite3.c" $BUILDDIR/pysqlcipher3/amalgamation
+cp "$BUILDDIR/sqlcipher/sqlite3.h" $BUILDDIR/pysqlcipher3/amalgamation
 
+if [[ ! -d "$PYSQLCIPHER_DIR/includes/sqlcipher" ]]; then
+  mkdir -p "$PYSQLCIPHER_DIR/includes/sqlcipher"
+fi
+
+cp "$BUILDDIR/sqlcipher/sqlite3.h" "$PYSQLCIPHER_DIR/includes/sqlcipher"
