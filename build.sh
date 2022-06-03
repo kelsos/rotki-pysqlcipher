ee92b2379613cbb175c8ba6abd3bc8bc69d2a0fc
Konstantinos Paparas <kelsos86@gmail.com>
2022-06-03 12:10:08 +0200

    good luck

--- build.sh
+++ build.sh
@@ -1,43 +1,36 @@
 #!/usr/bin/env bash
 
 WORKDIR=$PWD
+BUILDDIR=/tmp/pybuild
 
-if [[ -d "/tmp/pybuild/" ]]; then
-    echo "Warning /tmp/pybuild exists"
+if [[ -d "$BUILDDIR" ]]; then
+    echo "Warning $BUILDDIR exists"
     exit 1
 fi
 
 # Go into the directory and build sqlcipher
-echo "Compiling sqlcipher";
-mkdir -p /tmp/pybuild/sqlcipher
-cp -R sqlcipher /tmp/pybuild
-cd /tmp/pybuild//sqlcipher/ || exit 1
+echo "Creating sqlcipher amalgamation";
+mkdir -p "$BUILDDIR/sqlcipher"
+cp -R sqlcipher "$BUILDDIR"
+cd "$BUILDDIR/sqlcipher/" || exit 1
 
 ./configure \
   --enable-tempstore=yes \
   CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS" \
   LDFLAGS="-lcrypto"
 
-make
+make sqlite3.c
 
 cd "$WORKDIR" || exit 1
-cp -R pysqlcipher3 /tmp/pybuild
-cd /tmp/pybuild/pysqlcipher3 || exit 1
+cp -R pysqlcipher3 "$BUILDDIR"
+cd "$BUILDDIR/pysqlcipher3" || exit 1
 echo "Applying pysqlcipher3 setup patch"
 patch < "$WORKDIR/patches/pysqlcipher3_linux.diff"
 
-echo "Copying libsqlcipher"
-cp /tmp/pybuild/sqlcipher/.libs/libsqlcipher.so.0 /tmp/pybuild/pysqlcipher3/
-
-echo "Copying libcrypto"
-
-if [[ -f /usr/lib/libcrypto.so.1.1 ]]; then
-  cp /usr/lib/libcrypto.so.1.1 ../pysqlcipher3
-elif [[ -f /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 ]]; then
-  cp /usr/lib/x86_64-linux-gnu/libcrypto.so.1.1 ../pysqlcipher3
-else
-  echo "no libcrypto detected"
-  exit 1
+if [[ ! -d "$BUILDDIR/pysqlcipher3/amalgamation" ]]; then
+    mkdir "$BUILDDIR/pysqlcipher3/amalgamation"
 fi
 
+cp "$BUILDDIR/sqlcipher/sqlite3.c" $BUILDDIR/pysqlcipher3
+cp "$BUILDDIR/sqlcipher/sqlite3.h" $BUILDDIR/pysqlcipher3
 
