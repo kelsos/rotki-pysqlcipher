9cd655ff8e8f0a4fa261b29703f9c62ec64221d6
Konstantinos Paparas <kelsos86@gmail.com>
2022-06-03 23:12:43 +0200

    osx-3

--- prepare-osx.sh
+++ prepare-osx.sh
@@ -39,7 +39,8 @@ export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
 
 ./configure \
   --enable-tempstore=yes \
-  CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -I$(brew --prefix)/include" \
+  CFLAGS="-DSQLITE_HAS_CODEC -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -I/opt/homebrew/Cellar/openssl@1.1/1.1.1o/include" \
+  LDFLAGS="-L/opt/homebrew/Cellar/openssl@1.1/1.1.1o/lib"
 
 make sqlite3.c > /dev/null
 
